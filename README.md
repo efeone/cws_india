dc7f2701a06b5694a4b38528acc3ab8566235ea9
Muhammed Sinan K T <m.sinankt@gmail.com>
2023-09-13 12:15:48 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## CWS India
+
+App built on Frappe and ERPNext to support the operation in CWS India
+
+#### License
+
+MIT
---
+++ cws_india/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "CWS India",
+			"type": "module",
+			"label": _("CWS India")
+		}
+	]
