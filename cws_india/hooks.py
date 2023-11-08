0674683037b0cf9f40f9795520e39fadea0ebec7
Muhammed Sinan K T <muhammed786sinu000@gmail.com>
2023-11-08 17:21:56 +0530

    feat: Creating Donor and Donation from Lead

--- cws_india/hooks.py
+++ cws_india/hooks.py
@@ -120,13 +120,11 @@ doctype_js = {
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
-# }
+doc_events = {
+    "Lead": {
+        "on_update": "cws_india.cws_india.custom_scripts.lead.lead.create_donor"
+    }
+}
 
 # Scheduled Tasks
 # ---------------
