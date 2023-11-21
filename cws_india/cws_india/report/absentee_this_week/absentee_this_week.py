c7f66bc0e05e813fd769586a96808d41bdba3466
Muhammed Sinan K T <muhammed786sinu000@gmail.com>
2023-11-21 16:47:14 +0530

    feat: Report to view the employees absent in this week

---
+++ cws_india/cws_india/report/absentee_this_week/absentee_this_week.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2023, efeone and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+
+
+def execute(filters=None):
+	columns, data = get_columns(), get_data(filters)
+	return columns, data
+
+def get_columns():
+    return [
+        _("Employee") + ":Data:100",
+        _("Leave Application") + ":Data:150",
+		_("Employee Name") + ":Data:300",
+		_("Total Leave Days") + ":Float:150",
+		_("From Date") + ":Date:150",
+		_("To Date") + ":Date:150",
+	]
+    
+def get_data(filters):
+	data = []
+	today = frappe.utils.today()
+	first_day_of_this_week = frappe.utils.get_first_day_of_week(today)
+	last_day_of_this_week = frappe.utils.get_last_day_of_week(today)
+	data = frappe.db.sql("""
+						SELECT
+							employee,
+							name as leave_application,
+							employee_name,
+							total_leave_days,
+							from_date,
+							to_date
+						FROM
+							`tabLeave Application`
+						WHERE
+							status = "Approved"
+						AND
+							(
+								(from_date BETWEEN "{first_day_of_this_week}" AND "{last_day_of_this_week}")
+							OR
+								(to_date BETWEEN "{first_day_of_this_week}" AND "{last_day_of_this_week}")
+							)
+						ORDER BY
+							from_date asc
+						""".format(first_day_of_this_week=first_day_of_this_week, last_day_of_this_week=last_day_of_this_week),
+						as_dict=True
+					)
+	return data
